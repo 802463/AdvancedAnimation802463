9f237cd591910e99cbca9f2265f5ccf112d6a6b0
Greta <802463@seq.org>
2021-09-27 21:29:38 -0700

    Lab 0927: Orbiting
    movers/orbiters

---
+++ 0927/Orbiter.js
@@ -0,0 +1,31 @@
+function Orbiter(mover, oRad, oRad, angle, angleVel, clr, ctx) {
+  //variables
+  this.clr = clr;
+  this.rad = oRad;
+  this.context = ctx;
+  this.mover = mover;
+  this.rotator = new JSVector(oRad, 0);
+  this.rotator.setDirection(angle);
+  this.loc = JSVector.addGetNew(this.mover.loc, this.rotator);
+  this.angleVel = angleVel;
+}//++++++++++++++++++++++++++++++++ end orbit constructor
+
+//++++++++++++++++++++++++++++++++ methods
+  Orbiter.prototype.update = function(){
+    this.rotator.rotate(this.angleVel);
+    this.loc = JSVector.addGetNew(this.mover.loc, this.rotator);
+    }
+
+  Orbiter.prototype.draw = function(){
+    this.context.strokeStyle = this.clr;
+    this.context.lineWidth = 1;
+    this.context.beginPath();
+    this.context.arc(this.loc.x, this.loc.y, this.rad, Math.PI*2, 0, false);
+    this.context.stroke();
+
+    this.context.lineWidth = 1;
+    this.context.beginPath();
+    this.context.moveTo(this.mover.loc.x, this.mover.loc.y);
+    this.context.lineTo(this.loc.x, this.loc.y);
+    this.context.stroke();
+  }
---
+++ 0927/main.js
@@ -0,0 +1,56 @@
+//Greta Hachigian-Kreutzer
+//0909
+
+// wait for the page to finish loading with init as the callback
+window.addEventListener("load", init);
+
+// global variables
+var canvas, context;
+var movers = [];
+
+
+function init(){
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement
+    canvas = document.getElementById("cnv");
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+    context = canvas.getContext("2d");
+
+    let numMovers = 10;
+    for(var i = 0; i<numMovers;i++){
+      var x, y, dx, dy, rad, clr, r, g, b, numOrbiters;
+      rad = 7;
+      x = random(0,canvas.width);
+      y = random(0,canvas.height);
+      dx = Math.random()*2-1;
+      dy = Math.random()*2-1;
+      r = Math.random()*200+55;
+      g = Math.random()*155;
+      b = Math.random()*155;
+      clr = "rgba(" + r + ", "+ g + ","+ b +")"
+      numOrbiters = Math.floor(Math.random() * 10) + 3;
+      this.movers.push(new Mover(x, y, dx, dy, rad, clr, numOrbiters, context));
+    }
+
+      animate();
+}
+
+
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+
+// every animation cycle
+function animate() {
+    // erase the HTMLCanvasElement
+    context.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    context.fillRect(0,0,canvas.width,canvas.height);
+
+       for(let i = 0; i < this.movers.length; i++){
+         this.movers[i].run();
+       }
+
+    requestAnimationFrame(animate); // next cycle
+}
